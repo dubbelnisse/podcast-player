cc9758e2ed0e551fa40c034478772a69bcda5f26
Nils Nilsson <nilspontusnilsson@gmail.com>
2016-02-28 12:45:53 +0100

    Moved search to a component

--- App.js
+++ App.js
@@ -3,80 +3,19 @@
 import React, {
   Component,
   StyleSheet,
-  Text,
-  View,
-  Image,
-  ScrollView
+  View
 } from 'react-native'
 
-import Input from './lib/components/Input/Input'
-import Button from './lib/components/Button/Button'
-import SearchStore from './lib/stores/SearchStore'
-import SearchActions from './lib/actions/SearchActions'
-import connectToStores from 'alt-utils/lib/connectToStores'
+import Search from './lib/components/Search/Search'
 
 import Dimensions from 'Dimensions'
 const { width, height } = Dimensions.get('window')
 
-class App extends Component {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      searchTerm: ''
-    }
-
-    this._search = this._search.bind(this)
-    this._searchValue = this._searchValue.bind(this)
-  }
-
-  static getStores () {
-    return [SearchStore]
-  }
-
-  static getPropsFromStores () {
-    return SearchStore.getState()
-  }
-
-  _searchValue(name, value) {
-    this.setState({
-      searchTerm: value
-    })
-  }
-
-  _search () {
-    SearchActions.search(this.state.searchTerm)
-  }
-
-  _renderResult () {
-    if (this.props.searchResult.length === 0) { return null }
-
-    return this.props.searchResult.splice(0, 10).map((result, i) => {
-      console.log(result)
-      return (
-        <View
-          style={styles.podcast}
-          key={i}>
-          <Image
-            style={styles.image}
-            source={{uri: `${result.artworkUrl60}`}} />
-          <Text>{result.artistName}</Text>
-        </View>
-      )
-    })
-  }
-
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Input
-          onChange={this._searchValue} />
-        <Button
-          onPress={this._search}
-          text="search" />
-        <ScrollView style={styles.searchResult}>
-          {this._renderResult()}
-        </ScrollView>
+        <Search />
       </View>
     )
   }
@@ -89,18 +28,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 40,
     width: width,
-  },
-  searchResult: {
-    padding: 20
-  },
-  podcast: {
-    flexDirection: 'row',
-    marginBottom: 10
-  },
-  image: {
-    width: 60,
-    height: 60
   }
 })
-
-export default connectToStores(App)
